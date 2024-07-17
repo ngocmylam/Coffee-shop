09408c4783cd2b60b5438e10393d844f5b7fb853
Ngọc Mỹ Lâm <mylam6492@gmail.com>
2024-07-17 14:37:21 +0700

    cap nhat noi dung bai 2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import Homepage from './assets/pages/users/homepage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hello</h1>);
+root.render(<Homepage></Homepage>);
