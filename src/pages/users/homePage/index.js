12f3d098cd8e426e35c7d12d28237ebb3d6b365a
Ngọc Mỹ Lâm <mylam6492@gmail.com>
2024-08-06 15:37:21 +0700

    ls12

--- src/pages/users/homePage/index.js
+++ src/pages/users/homePage/index.js
@@ -1,11 +1,73 @@
 import { memo } from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import "./style.scss";
+import cat1Img from "../../../../src/assets/users/images/categories/cat-1.jpg";
+import cat2Img from "../../../../src/assets/users/images/categories/cat-2.jpg";
+import cat3Img from "../../../../src/assets/users/images/categories/cat-3.jpg";
+import cat4Img from "../../../../src/assets/users/images/categories/cat-4.jpg";
+import cat5Img from "../../../../src/assets/users/images/categories/cat-5.jpg";
 
 const HomePage = () => {
-    return (
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5  
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 4
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
+
+      const sliderItems = [
+        {
+            bgImg: cat1Img,
+            name: "Cam tươi"
+        },
+        {
+            bgImg: cat2Img,
+            name: "Hoa quả khô"
+        },
+        {
+            bgImg: cat3Img,
+            name: "Rau củ tươi"
+        },
+        {
+            bgImg: cat4Img,
+            name: "Sữa hộp"
+        },
+        {
+            bgImg: cat5Img,
+            name: "Thịt bò"
+        }
+      ]
+
+      return (
         <>
-            <h1>HomePage</h1>
+            {/*Categories Begin*/}
+                <div className="container__categories_slider container">
+                    <Carousel responsive={responsive} className="categories_slider">
+                        {sliderItems.map((item,key) => (
+                          <div className="categories_slider_item"
+                          style={{ backgroundImage: `url(${item.bgImg})`}}>
+                            <p>{item.name}</p>
+                          </div>
+                        ))}
+                    </Carousel>
+                </div>
+            {/*Categories End*/}
         </>
-    )
+      )
 };
 
 export default memo(HomePage);
